073efc6cc6a10eaeaff541de5d1a2c1a775acba6
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-08-26 18:46:30 +0530

    campturing images using camera module

--- face_detection_attendance.py
+++ face_detection_attendance.py
@@ -6,3 +6,14 @@ from datetime import datetime  # To get the system's Date and time
 import time
 import httplib2
 http = httplib2.Http()
+
+path = 'image_folder'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)   
+    classNames.append(os.path.splitext(cl)[0])  
+print(classNames)
