b38e107a03bfa627bef51cf52046fe2501077bd9
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-08-26 18:45:39 +0530

    Initializinig files

---
+++ face_detection_attendance.py
@@ -0,0 +1,8 @@
+import cv2        #openCV
+import numpy as np  # supportinig Lib for OpenCV
+import face_recognition   #To Create Recognizer
+import os  #To access Operating System
+from datetime import datetime  # To get the system's Date and time
+import time
+import httplib2
+http = httplib2.Http()
