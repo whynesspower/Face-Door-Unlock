999f6074ad047c4a6ead7b680ca6f004da647b52
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-08-26 18:53:17 +0530

    Setup code for arduino

--- ArduinoCode/arduino_server_code.ino
+++ ArduinoCode/arduino_server_code.ino
@@ -7,3 +7,22 @@ uint8_t pin_led = D0;
 char* ssid = "Yashraj Shukla";
 char* password = "12345678";
 Servo myservo; 
+void setup()
+{
+  myservo.attach(D4);
+  pinMode(pin_led, OUTPUT);
+  WiFi.begin(ssid,password);
+  Serial.begin(115200);
+  while(WiFi.status()!=WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/",[](){server.send(200,"text/plain","Hello World!");});
+  server.on("/toggle",toggleLED);
+  server.begin();
+}
