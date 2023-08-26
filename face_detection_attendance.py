9ae562a26333129a4105b9c3b103d847d56ef1b2
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-08-26 18:47:42 +0530

    encoding the images and initilizing the camera module

--- face_detection_attendance.py
+++ face_detection_attendance.py
@@ -17,3 +17,16 @@ for cl in myList:
     images.append(curImg)   
     classNames.append(os.path.splitext(cl)[0])  
 print(classNames)
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+
+cap = cv2.VideoCapture(0)
